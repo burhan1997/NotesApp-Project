8c8f73ab469178081f305f1fc83042b1003a41b5
burhan1997 <burhanelaldi97@gmail.com>
2024-03-14 00:24:51 +0100

    about route is added

--- server/controllers/mainController.js
+++ server/controllers/mainController.js
@@ -7,6 +7,18 @@ exports.homepage = async (req, res) => {
       title: "NodeJs Notes",
       description: "Free NodeJs Notes App",
     };
-
   res.render('index', locals);
+}
+
+
+/**
+ * GET /
+ * About
+ */
+exports.about = async (req, res) => {
+    const locals = {
+      title: "About - NodeJs Notes",
+      description: "Free NodeJs Notes App",
+    };
+  res.render('about', locals);
 }
